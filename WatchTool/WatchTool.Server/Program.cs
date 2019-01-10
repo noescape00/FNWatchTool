125ad2b2b1798c805644fbe43ac120d999172dc8
noescape0 <NoEscape0@ya.ru>
2019-01-10 16:28:16 +0300

    logging and DI for server

--- WatchTool/WatchTool.Server/Program.cs
+++ WatchTool/WatchTool.Server/Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace WatchTool.Server
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private ServerApplication application;
+
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            new Program().MainAsync(args).GetAwaiter().GetResult();
+        }
+
+        private async Task MainAsync(string[] args)
+        {
+            Console.CancelKeyPress += this.ShutdownHandler;
+
+            this.application = new ServerApplication();
+            await this.application.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        /// <summary>Shutdown the handler. Executed when user presses CTRL+C on console.</summary>
+        private void ShutdownHandler(object sender, ConsoleCancelEventArgs args)
+        {
+            this.application.Dispose();
+
+            args.Cancel = true;
+            Environment.Exit(0);
         }
     }
 }
