303aa1e2ab03bb967d272dac4b4a3fc30431b171
noescape0 <NoEscape0@ya.ru>
2019-01-10 15:29:12 +0300

    Init solution

---
+++ WatchTool/WatchTool.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WatchTool.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
