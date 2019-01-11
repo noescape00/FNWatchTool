ddad3fcea757f2f9a92306e06b7adc1673c343be
noescape0 <NoEscape0@ya.ru>
2019-01-11 16:49:33 +0300

    info payload

--- WatchTool/WatchTool.Common/P2P/Payloads/NodeInfoPayload.cs
+++ WatchTool/WatchTool.Common/P2P/Payloads/NodeInfoPayload.cs
@@ -6,10 +6,30 @@ namespace WatchTool.Common.P2P.Payloads
     [Payload("info")]
     public class NodeInfoPayload : Payload
     {
-        /// <summary>Hash of the latest commit or <c>null</c> if repo was not cloned yet.</summary>
+        public bool IsNodeCloned { get; set; }
+
+        /// <summary>Info about node's repo or <c>null</c> if <see cref="IsNodeCloned"/> is <c>false</c>.</summary>
+        public NodeRepositoryVersionInfo NodeRepoInfo { get; set; }
+
+        public bool IsNodeRunning { get; set; }
+
+        /// <summary>Info about node's running state or <c>null</c> if <see cref="IsNodeRunning"/> is <c>false</c>.</summary>
+        public RunningNodeInfo RunningNodeInfo { get; set; }
+    }
+
+    public class NodeRepositoryVersionInfo
+    {
+        /// <summary>Hash of the latest commit.</summary>
         public string LatestCommitHash { get; set; }
 
-        /// <summary>Date of the latest commit or <c>null</c> if repo was not cloned yet.</summary>
-        public DateTime? LatestCommitDate { get; set; }
+        /// <summary>Date of the latest commit.</summary>
+        public DateTime LatestCommitDate { get; set; }
+    }
+
+    public class RunningNodeInfo
+    {
+        public int ConsensusHeight { get; set; }
+
+        public bool IsStaking { get; set; }
     }
 }
