a907c9d6f01ff9317f7107206147378118a5e78c
noescape0 <NoEscape0@ya.ru>
2019-01-12 16:54:02 +0300

    test data

--- WatchTool/WatchTool.Server.Dashboard/Controllers/HomeController.cs
+++ WatchTool/WatchTool.Server.Dashboard/Controllers/HomeController.cs
@@ -1,6 +1,10 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net;
 using Microsoft.AspNetCore.Mvc;
 using WatchTool.Common.Models;
+using WatchTool.Common.P2P.Payloads;
 using WatchTool.Server.Dashboard.Models;
 
 namespace WatchTool.Server.Dashboard.Controllers
@@ -9,16 +13,39 @@ namespace WatchTool.Server.Dashboard.Controllers
     {
         private readonly IPeersInformationModelProvider peersInfoProvider;
 
-        public HomeController(IPeersInformationModelProvider peersInfoProvider)
+        public HomeController(IPeersInformationModelProvider peersInfoProvider = null) // TODO not null
         {
             this.peersInfoProvider = peersInfoProvider;
         }
 
         public IActionResult Index()
         {
-            PeersInformationModel infoModel = this.peersInfoProvider.GetPeersInfo();
+            //PeersInformationModel infoModel = this.peersInfoProvider.GetPeersInfo();
 
-            return View(infoModel);
+
+            var peerInfo = new PeerInfoModel()
+            {
+                EndPoint = new IPEndPoint(15134635, 19879),
+                LatestInfoPayload = new NodeInfoPayload()
+                {
+                    IsNodeCloned = true,
+                    IsNodeRunning = false,
+                    NodeRepoInfo = new NodeRepositoryVersionInfo()
+                    {
+                        LatestCommitDate = DateTime.Now,
+                        LatestCommitHash = "hashhashhashhashhashhashhashhashhashhash"
+                    }
+                }
+            };
+
+            PeersInformationModel fakeModel = new PeersInformationModel();
+            fakeModel.PeersInfo = new List<PeerInfoModel>()
+            {
+                peerInfo, peerInfo
+            };
+
+
+            return View(fakeModel);
         }
 
         public IActionResult About()
